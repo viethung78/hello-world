73a8d75c39edf806b37e68ba5f5bdface0943f26
DAO VIET HUNG <IFASHIONUP@GMAIL.COM>
2023-04-11 14:43:36 +0700

    Update htlm, css, js

---
+++ script.js
@@ -0,0 +1,83 @@
+<!-- layout -->
+    <link rel="stylesheet" href="layout.css">
+
+    <!-- component: header, nav, main, article, hightlight, footer -->
+    <link rel="stylesheet" href="header.css">
+    <link rel="stylesheet" href="nav.css">
+    <link rel="stylesheet" href="article.css">
+    <link rel="stylesheet" href="footer.css">
+
+    <link rel="stylesheet" href="style.css"></link>
+
+    let a = 1;
+let b = 2;
+let c = a + b;
+
+// variables - containers for storing data
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// variables can be declared 4 ways
+// const --> contanst, don't be changed
+// let --> changeable value
+// var --> old way, don't do this
+
+// const myName = 'Khoan';
+// console.log(myName);
+
+// let myFavColor = 'Blue';
+// console.log(myFavColor);
+// myFavColor = 'purple';
+// console.log(myFavColor);
+
+// DATA TYPES
+// string --> 'text' or "text" or '2' or "2"
+// number/integers --> 2 or 42 or 123345
+// boolean --> true or false
+
+
+// console.log('Hello world!');
+// console.log(32 + 8);
+// console.log(false);
+// console.log(3 + 2 == 4);
+
+// Useful function
+// console.log() --> print infomation to the console
+// alert() --> give us a popup box
+// prompt()  --> how you can get info from the user
+// document.write()  --> how you will put content on your website
+
+// Conditional logic (if/else)
+// let userName = prompt('What is your name?');
+// console.log('userName=',userName);
+
+// if (userName == 'Khoan'){
+//   // Code in here
+//   console.log('Hiya teach!');
+// } else if (userName == 'Hoa') {
+//   alert("TA's are some");
+// } else {
+//   alert('Hello and welcome, class');
+// } 
+
+// Operators
+// let myNumber = 42; // number/integer
+
+// console.log(myNumber == 42); // true
+// console.log(myNumber == '42'); // number & string
+// console.log(myNumber === 42); // false
+
+
+// let response = prompt('typing your password?');
+// console.log('response', response);
+
+// if (response === 6){
+//   console.log('Right!!!');
+// } else {
+//   console.log('Fail!!!'); // case 
+// }
+
+// console.log(typeof(response));
